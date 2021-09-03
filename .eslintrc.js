39a73d8ddc49deb778213c10ee73eb8389be6e83
Ron <funccoder@gmail.com>
2021-09-03 18:16:05 +0900

    Add Base Code

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['google', 'plugin:prettier/recommended', 'plugin:react/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'react'],
+  rules: {
+    'require-jsdoc': 'off',
+
+    // import
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        groups: ['builtin', 'external', 'internal', 'unknown', ['parent', 'sibling', 'index']],
+        pathGroups: [
+          {
+            pattern: 'share/**',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: 'service/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    jsxBracketSameLine: true,
+    bracketSpacing: true,
+    endOfLine: 'auto',
+  }
+  
