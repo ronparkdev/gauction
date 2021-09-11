3032db47aff185821640575493508a663b02ec0c
Ron <funccoder@gmail.com>
2021-09-11 17:17:20 +0900

    Implement overlay (WIP)

--- packages/service/src/components/Map/utils.ts
+++ packages/service/src/components/Map/utils.ts
@@ -8,6 +8,8 @@ import { MapState } from './state'
 
 const getKakaoMapLibrary = () => (window as any).kakao.maps
 
+const getItemMap = (): { [key in string]: ListItem } => mapRawData
+
 const createKakaoMap = (container) => {
   const KakaoMap = getKakaoMapLibrary()
 
@@ -29,40 +31,91 @@ const createKakaoMap = (container) => {
   return map
 }
 
-const updateMarker = (() => {
-  let isInitialized = false
-  const itemMap: { [key in string]: ListItem } = mapRawData
-  const markerMap: { [key in string]: any } = {}
+const getCachedInfoWindow = (() => {
+  const itemMap = getItemMap()
+  const infoWindowMap: { [key: string]: any } = {}
 
-  return (map, filter: FilterRangeState) => {
-    if (!isInitialized) {
-      const KakaoMap = getKakaoMapLibrary()
+  const createCloseOverlayHandler = (key) => () => getCachedInfoWindow(key).close()
 
-      const invalidKeys: string[] = []
-
-      Object.keys(itemMap).forEach((key) => {
-        const item = itemMap[key]
-
-        if (!item || !item.geo || markerMap[key]) {
-          invalidKeys.push(key)
-          return
-        }
-
-        const position = new KakaoMap.LatLng(item.geo.latitude, item.geo.longitude)
-
-        const marker = new KakaoMap.Marker({
-          position,
-          clickable: true,
-        })
-
-        markerMap[key] = marker
-      })
-
-      console.error('invalid keys', invalidKeys)
-
-      isInitialized = true
+  return (key) => {
+    const item = itemMap[key]
+    const cachedInfoWindow = infoWindowMap[key]
+    if (cachedInfoWindow) {
+      return cachedInfoWindow
     }
-    const bounds = map.getBounds()
+
+    const KakaoMap = getKakaoMapLibrary()
+
+    const content =
+      '<div class="wrap">' +
+      '    <div class="info">' +
+      '        <div class="title">' +
+      `            ${'테스트'}` +
+      `            <div class="close" onclick="${createCloseOverlayHandler(key)}" title="닫기"></div>` +
+      '        </div>' +
+      '        <div class="body">' +
+      '            <div class="img">' +
+      '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
+      '           </div>' +
+      '            <div class="desc">' +
+      `                <div class="ellipsis">${item.address}</div>` +
+      '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+      '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+      '            </div>' +
+      '        </div>' +
+      '    </div>' +
+      '</div>'
+
+    const infoWindow = new KakaoMap.InfoWindow({
+      content,
+    })
+
+    infoWindowMap[key] = infoWindow
+
+    return infoWindow
+  }
+})()
+
+const createMarkerMap = (kakaoMap) => {
+  const KakaoMap = getKakaoMapLibrary()
+  const itemMap = getItemMap()
+
+  return Object.keys(itemMap).reduce((map, key) => {
+    const item = itemMap[key]
+
+    if (!item || !item.geo || map[key]) {
+      return map
+    }
+
+    const position = new KakaoMap.LatLng(item.geo.latitude, item.geo.longitude)
+
+    const marker = new KakaoMap.Marker({
+      position,
+      clickable: true,
+    })
+
+    KakaoMap.event.addListener(marker, 'mouseover', () => {
+      console.log(key, 'mouseover')
+      getCachedInfoWindow(key).open(kakaoMap, marker)
+    })
+    KakaoMap.event.addListener(marker, 'mouseout', () => {
+      console.log(key, 'mouseout')
+      getCachedInfoWindow(key).close()
+    })
+
+    map[key] = marker
+    return map
+  }, {})
+}
+
+const updateMarker = (() => {
+  let cachedMarkerMap: { [key in string]: any }
+  const itemMap = getItemMap()
+
+  return (kakaoMap, filter: FilterRangeState) => {
+    const markerMap = cachedMarkerMap || (() => (cachedMarkerMap = createMarkerMap(kakaoMap)))()
+
+    const bounds = kakaoMap.getBounds()
     const swLatLng = bounds.getSouthWest()
     const neLatLng = bounds.getNorthEast()
 
@@ -93,7 +146,7 @@ const updateMarker = (() => {
         filter.pricePerArea.min <= pricePerArea / 10_000 &&
         filter.pricePerArea.max >= pricePerArea / 10_000
 
-      marker.setMap(isVisible ? map : null)
+      marker.setMap(isVisible ? kakaoMap : null)
     })
   }
 })()
