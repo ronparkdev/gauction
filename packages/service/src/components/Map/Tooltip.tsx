c38086d79ab08e9ce07f1805ad10ff11d2ac4cae
funccoder <funccoder@gmail.com>
2022-04-27 11:10:44 +0900

    툴팁 내용물 구현

--- packages/service/src/components/Map/Tooltip.tsx
+++ packages/service/src/components/Map/Tooltip.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import ReactDOM from 'react-dom'
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 
+import { configSizeUnitState } from 'service/stores/atoms/configState'
 import { selectedListItemState } from 'service/stores/atoms/select'
+import SizeUnit from 'service/types/sizeUnit'
+import { StringUtils } from 'service/utils/string'
 
 export const Tooltip = () => {
   const [selectedItem] = useRecoilState(selectedListItemState)
+  const sizeUnit = useRecoilValue(configSizeUnitState)
 
-  if (!selectedItem) {
+  const tableData = useMemo(() => {
+    if (!selectedItem) {
+      return null
+    }
+
+    const appraisedPrice = selectedItem.appraisedPrice && StringUtils.getReadablePrice(selectedItem.appraisedPrice)
+    const lowPrice = StringUtils.getReadablePrice(selectedItem.lowPrice)
+    const percentOfAppraisedPrice =
+      selectedItem.appraisedPrice && `(${Math.floor((selectedItem.lowPrice / selectedItem.appraisedPrice) * 100)}%)`
+    const groundSize = `${Math.floor(SizeUnit.convert(sizeUnit, selectedItem.groundSize))}${SizeUnit.getName(sizeUnit)}`
+    const pricePerGroundSize = StringUtils.getReadablePrice(
+      Math.floor(selectedItem.lowPrice / SizeUnit.convert(sizeUnit, selectedItem.groundSize)),
+    )
+
+    return {
+      감정가: appraisedPrice,
+      현재가: [lowPrice, percentOfAppraisedPrice].filter((str) => !!str).join(' '),
+      토지크기: groundSize,
+      [`${SizeUnit.getName(sizeUnit)}단가`]: pricePerGroundSize,
+      // RAW: JSON.stringify(selectedItem),
+    }
+  }, [selectedItem, sizeUnit])
+
+  if (!selectedItem || !tableData) {
     return null
   }
 
@@ -16,5 +43,20 @@ export const Tooltip = () => {
     return null
   }
 
-  return ReactDOM.createPortal(<div>hello {JSON.stringify(selectedItem)}</div>, modalRoot)
+  return ReactDOM.createPortal(
+    <div style={{ position: 'absolute', bottom: -20, background: 'white', width: 300 }}>
+      <table>
+        {Object.keys(tableData).map((key) => {
+          const value = tableData[key]
+          return (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+            </tr>
+          )
+        })}
+      </table>
+    </div>,
+    modalRoot,
+  )
 }
